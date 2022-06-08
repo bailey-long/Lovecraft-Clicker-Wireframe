537827d2430127988008b4840ee1228a94e06bbd
Loopypew <fivebailey2@gmail.com>
2022-06-08 21:22:36 +1200

    Add project files.

---
+++ ClickerGame/form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace ClickerGame
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.currencyDisplay = new System.Windows.Forms.Label();
+            this.clickingObject = new System.Windows.Forms.Button();
+            this.rewardLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.reward3 = new System.Windows.Forms.Label();
+            this.reward2 = new System.Windows.Forms.Label();
+            this.reward1 = new System.Windows.Forms.Label();
+            this.upgrade3 = new System.Windows.Forms.Button();
+            this.upgrade2 = new System.Windows.Forms.Button();
+            this.upgrade1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.station1 = new System.Windows.Forms.Button();
+            this.station2 = new System.Windows.Forms.Button();
+            this.station3 = new System.Windows.Forms.Button();
+            this.station4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.addcap1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // currencyDisplay
+            // 
+            this.currencyDisplay.AutoSize = true;
+            this.currencyDisplay.Font = new System.Drawing.Font("MS Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currencyDisplay.Location = new System.Drawing.Point(22, 44);
+            this.currencyDisplay.Name = "currencyDisplay";
+            this.currencyDisplay.Size = new System.Drawing.Size(119, 13);
+            this.currencyDisplay.TabIndex = 3;
+            this.currencyDisplay.Text = "Favour Details";
+            // 
+            // clickingObject
+            // 
+            this.clickingObject.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.clickingObject.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.clickingObject.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clickingObject.Font = new System.Drawing.Font("MS Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clickingObject.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.clickingObject.ImageKey = "(none)";
+            this.clickingObject.Location = new System.Drawing.Point(25, 108);
+            this.clickingObject.Name = "clickingObject";
+            this.clickingObject.Size = new System.Drawing.Size(151, 124);
+            this.clickingObject.TabIndex = 0;
+            this.clickingObject.Text = "Perform Ritual";
+            this.clickingObject.UseVisualStyleBackColor = true;
+            this.clickingObject.Click += new System.EventHandler(this.ritualBox_Click);
+            // 
+            // rewardLabel
+            // 
+            this.rewardLabel.AutoSize = true;
+            this.rewardLabel.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.rewardLabel.Location = new System.Drawing.Point(3, 0);
+            this.rewardLabel.Name = "rewardLabel";
+            this.rewardLabel.Size = new System.Drawing.Size(113, 12);
+            this.rewardLabel.TabIndex = 5;
+            this.rewardLabel.Text = "Sacrificial Boons:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.reward3);
+            this.panel1.Controls.Add(this.reward2);
+            this.panel1.Controls.Add(this.reward1);
+            this.panel1.Controls.Add(this.rewardLabel);
+            this.panel1.Font = new System.Drawing.Font("MS Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(234, 117);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(269, 157);
+            this.panel1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 24);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "[40 +] sacrifices:  \r\nbetter Shogoths";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // reward3
+            // 
+            this.reward3.AutoSize = true;
+            this.reward3.Location = new System.Drawing.Point(3, 48);
+            this.reward3.Name = "reward3";
+            this.reward3.Size = new System.Drawing.Size(119, 24);
+            this.reward3.TabIndex = 8;
+            this.reward3.Text = "[25 +} sacrifices: \r\n+100 per ritual\r\n";
+            // 
+            // reward2
+            // 
+            this.reward2.AutoSize = true;
+            this.reward2.Location = new System.Drawing.Point(128, 17);
+            this.reward2.Name = "reward2";
+            this.reward2.Size = new System.Drawing.Size(119, 24);
+            this.reward2.TabIndex = 7;
+            this.reward2.Text = "[15 +} sacrifices: \r\nbetter cultists\r\n";
+            this.reward2.Click += new System.EventHandler(this.reward2_Click);
+            // 
+            // reward1
+            // 
+            this.reward1.AutoSize = true;
+            this.reward1.Location = new System.Drawing.Point(3, 17);
+            this.reward1.Name = "reward1";
+            this.reward1.Size = new System.Drawing.Size(119, 24);
+            this.reward1.TabIndex = 6;
+            this.reward1.Text = "[10 +] sacrifices: \r\n+20 per ritual";
+            // 
+            // upgrade3
+            // 
+            this.upgrade3.Font = new System.Drawing.Font("MS Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.upgrade3.Image = global::ClickerGame.Properties.Resources.ancient_zyme;
+            this.upgrade3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.upgrade3.Location = new System.Drawing.Point(509, 104);
+            this.upgrade3.Name = "upgrade3";
+            this.upgrade3.Size = new System.Drawing.Size(230, 54);
+            this.upgrade3.TabIndex = 7;
+            this.upgrade3.Text = "Summon Shogoth:";
+            this.upgrade3.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.upgrade3.UseVisualStyleBackColor = true;
+            this.upgrade3.Click += new System.EventHandler(this.upgrade3_Click);
+            // 
+            // upgrade2
+            // 
+            this.upgrade2.Font = new System.Drawing.Font("MS Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.upgrade2.Image = global::ClickerGame.Properties.Resources.blood_puddle_red;
+            this.upgrade2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.upgrade2.Location = new System.Drawing.Point(234, 44);
+            this.upgrade2.Name = "upgrade2";
+            this.upgrade2.Size = new System.Drawing.Size(269, 54);
+            this.upgrade2.TabIndex = 4;
+            this.upgrade2.Text = "Sacrifice Cultist:";
+            this.upgrade2.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.upgrade2.UseVisualStyleBackColor = true;
+            this.upgrade2.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // upgrade1
+            // 
+            this.upgrade1.Font = new System.Drawing.Font("MS Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.upgrade1.Image = global::ClickerGame.Properties.Resources.wizard;
+            this.upgrade1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.upgrade1.Location = new System.Drawing.Point(509, 44);
+            this.upgrade1.Name = "upgrade1";
+            this.upgrade1.Size = new System.Drawing.Size(230, 54);
+            this.upgrade1.TabIndex = 2;
+            this.upgrade1.Text = "Enlist Cultist: ";
+            this.upgrade1.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.upgrade1.UseVisualStyleBackColor = true;
+            this.upgrade1.Click += new System.EventHandler(this.upgradeOne_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(507, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Cultist Workstations:\r\n";
+            // 
+            // station1
+            // 
+            this.station1.Location = new System.Drawing.Point(509, 189);
+            this.station1.Name = "station1";
+            this.station1.Size = new System.Drawing.Size(230, 51);
+            this.station1.TabIndex = 9;
+            this.station1.Text = "Ritual Altar";
+            this.station1.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.station1.UseVisualStyleBackColor = true;
+            // 
+            // station2
+            // 
+            this.station2.Location = new System.Drawing.Point(509, 246);
+            this.station2.Name = "station2";
+            this.station2.Size = new System.Drawing.Size(230, 54);
+            this.station2.TabIndex = 10;
+            this.station2.Text = "button1";
+            this.station2.UseVisualStyleBackColor = true;
+            // 
+            // station3
+            // 
+            this.station3.Location = new System.Drawing.Point(509, 306);
+            this.station3.Name = "station3";
+            this.station3.Size = new System.Drawing.Size(230, 54);
+            this.station3.TabIndex = 11;
+            this.station3.Text = "button2";
+            this.station3.UseVisualStyleBackColor = true;
+            // 
+            // station4
+            // 
+            this.station4.Location = new System.Drawing.Point(509, 366);
+            this.station4.Name = "station4";
+            this.station4.Size = new System.Drawing.Size(230, 54);
+            this.station4.TabIndex = 12;
+            this.station4.Text = "button3";
+            this.station4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(509, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(41, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(556, 217);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(41, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // addcap1
+            // 
+            this.addcap1.Location = new System.Drawing.Point(603, 217);
+            this.addcap1.Name = "addcap1";
+            this.addcap1.Size = new System.Drawing.Size(136, 23);
+            this.addcap1.TabIndex = 16;
+            this.addcap1.Text = "Buy";
+            this.addcap1.UseVisualStyleBackColor = true;
+            this.addcap1.Click += new System.EventHandler(this.addcap1_Click);
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 450);
+            this.Controls.Add(this.addcap1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.station4);
+            this.Controls.Add(this.station3);
+            this.Controls.Add(this.station2);
+            this.Controls.Add(this.station1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.upgrade3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.upgrade2);
+            this.Controls.Add(this.currencyDisplay);
+            this.Controls.Add(this.upgrade1);
+            this.Controls.Add(this.clickingObject);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.Name = "form1";
+            this.Text = "Lovecraft Clicker";
+            this.Load += new System.EventHandler(this.form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clickingObject;
+        private System.Windows.Forms.Button upgrade1;
+        private System.Windows.Forms.Label currencyDisplay;
+        private System.Windows.Forms.Button upgrade2;
+        private System.Windows.Forms.Label rewardLabel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label reward1;
+        private System.Windows.Forms.Label reward2;
+        private System.Windows.Forms.Button upgrade3;
+        private System.Windows.Forms.Label reward3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button station1;
+        private System.Windows.Forms.Button station2;
+        private System.Windows.Forms.Button station3;
+        private System.Windows.Forms.Button station4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button addcap1;
+    }
+}
+
---
+++ ClickerGame/form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClickerGame
+{
+    public partial class form1 : Form
+    {
+        //Timer setups
+        private readonly Timer _timer = new Timer();
+        private readonly Timer _timer2 = new Timer();
+        private readonly Timer _ritualTimer = new Timer();
+        //initial variables
+        double favour = 0;
+        double favourPerSecond = 0;
+        double ritualFavour = 10;
+        int ritualLength = 10;
+        int ritualCounter = 0;
+        //list of upgrade costs
+        double _costofOne = 10;
+        double _costofThree = 5000;
+        //list of upgrade purchase counters
+        int _numofOne = 0;
+        int _numofTwo = 0;
+        int _numofThree = 0;
+        //initialise upgrade effect
+        double _effectofOne = 0;
+        double _effectofTwo = 0;
+        double _effectofThree = 0;
+        //Sacrifice reward status
+        bool _rewOne = false;
+        bool _rewTwo = false;
+        bool _rewThree = false;
+        bool _rewFour = false;
+        double _upgradesOne = 0;
+        double _upgradesTwo = 0;
+        double _upgradesThree = 0;
+        //workstation status setup
+        int _workinOne = 0;
+        int _spaceinOne = 0;
+        double _spacecostofOne = 100;
+        public form1()
+        {
+            InitializeComponent();
+            //Timer to add favour from income sources every second
+            _timer2.Interval = 1000;
+            _timer2.Tick += UpdateFavour;
+            _timer2.Enabled = true;
+            //Timer to update UI details
+            _timer.Interval = 1;
+            _timer.Tick += UpdateFields;
+            _timer.Enabled = true;
+            //set clickingobject color to green
+            clickingObject.BackColor = Color.Green;
+        }
+        void UpdateFavour(object sender, EventArgs e)
+        {
+            //update upgrade effects
+            _effectofOne = _numofOne * (0.5 + _upgradesOne);
+            _effectofTwo = _numofTwo * (1 + _upgradesTwo);
+            _effectofThree = _numofThree * (15 + _upgradesThree);
+            //Keeps track of total favour per second from ALL sources
+            favourPerSecond = (_effectofOne + _effectofTwo + _effectofThree);
+            //Adds favourPerSecond to favour
+            favour += favourPerSecond;
+            //Removes unnecessary decimal places
+            Math.Round(favour, 1);
+        }
+        void UpdateFields(object sender, EventArgs e)
+        {
+            //cultist sacrifice reward condition checker
+            if (!_rewOne && _numofTwo >= 10)
+            {
+                _rewOne = true;
+                ritualFavour += 20;
+
+            }
+            if(!_rewTwo && _numofTwo >= 15)
+            {
+                _rewTwo = true;
+                _upgradesOne = 1;
+
+            }
+            if(!_rewThree && _numofTwo >= 25)
+            {
+                _rewThree = true;
+                ritualFavour += 100;
+            }
+            if(!_rewFour && _numofTwo >= 40)
+            {
+                _rewFour = true;
+                _upgradesThree = 15;
+            //Show if player can afford upgrades
+            if(favour >= _costofOne)
+            {
+                upgrade1.BackColor = Color.Green;
+            } else
+            {
+                upgrade1.BackColor = Color.Empty;
+            }
+            if (_numofOne >= 1)
+            {
+                upgrade2.BackColor = Color.Green;
+            }
+            else
+            {
+                upgrade2.BackColor = Color.Empty;
+            }
+            if (favour >= _costofThree)
+            {
+                upgrade3.BackColor = Color.Green;
+            }
+            else
+            {
+                upgrade3.BackColor = Color.Empty;
+            }
+            //Show if player can afford workstations.
+            if(favour >= _spacecostofOne)
+            {
+                addcap1.BackColor = Color.Green;
+            }
+            else
+            {
+                addcap1.BackColor = Color.Empty;
+            }
+            }
+            //FAVOUR STATS
+            //Holds information relating to favour
+            currencyDisplay.Text = ($"Eldritch Favour: {favour} \nFavour Per Second: {favourPerSecond} \nFavour per ritual: {ritualFavour}\n");
+            //Display upgrade box texts
+            upgrade1.Text = ($"{_numofOne} owned \nEnlist Cultist: {_costofOne} favour \nfavour per second: {(0.5 + _upgradesOne)}");
+            upgrade2.Text = ($"Cultist Sacrificed: {_numofTwo} \nSacrice Cultist? \nfavour per ritual: + 1");
+            upgrade3.Text = ($"Shoggoths summoned: {_numofThree} \nSummon Shogoth: {_costofThree} favour \nfavour per second: {10 + _upgradesThree}");
+            //Workstation texts
+            station1.Text = ($"Ritual Altar: Removes time from the ritual \nWorkers: {_workinOne} Space: {_spaceinOne}");
+            addcap1.Text = ($"Add Space: {_spacecostofOne}");
+
+        }
+
+        private void ritualBox_Click(object sender, EventArgs e)
+        {
+            //begin ritual process
+            if (!_ritualTimer.Enabled)
+            {
+                clickingObject.BackColor = Color.Empty;
+                //setup counter
+                ritualCounter = ritualLength;
+                //Ritual timer stopwatch
+                _ritualTimer.Interval = 1000;
+                clickingObject.Text = ("Starting Ritual");
+                _ritualTimer.Tick += completeRitual;
+                _ritualTimer.Enabled = true;
+            }
+        }
+        private void completeRitual(object sender, EventArgs e)
+        {
+            //if ritual is complete
+            if(ritualCounter <= 0)
+            {
+                clickingObject.BackColor = Color.Green;
+                _ritualTimer.Tick -= completeRitual;
+                _ritualTimer.Enabled = false;
+                clickingObject.Text = ("Perform Ritual");
+                favour += ritualFavour;
+            }
+            else if (_ritualTimer.Enabled)
+            {
+                clickingObject.BackColor = Color.Empty;
+                clickingObject.Text = ($"Performing Ritual: {ritualCounter}");
+                ritualCounter--;
+            }
+        }
+        private void upgradeOne_Click(object sender, EventArgs e)
+        {
+            if(favour >= _costofOne)
+            {
+                favour -= _costofOne;
+                //Make upgrade cost more favour
+                _costofOne = Math.Ceiling(_costofOne * 1.5);
+                _numofOne++;
+            } 
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //if player has a cultist add 1 to cultists sacrificed
+            if(_numofOne >= 1)
+            {
+                ritualFavour += 1;
+                _numofOne--;
+                _numofTwo++;
+            }
+        }
+
+        private void upgrade3_Click(object sender, EventArgs e)
+        {
+            if (favour >= _costofThree)
+            {
+                favour -= _costofThree;
+                //Make upgrade cost more favour
+                _costofThree = Math.Ceiling(_costofThree * 1.5);
+                _numofThree++;
+            }
+        }
+        //Click functions to add workers to stations.
+        //Workstation 1, ritual is shorter for each worker
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            if(_numofOne >= 1 && _workinOne < _spaceinOne)
+            {
+                ritualLength--;
+                _workinOne++;
+                _numofOne--;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(_workinOne >= 1)
+            {
+                ritualLength++;
+                _workinOne--;
+                _numofOne++;
+            }
+        }
+
+        private void addcap1_Click(object sender, EventArgs e)
+        {
+            if(favour >= _spacecostofOne){
+                favour -= _spacecostofOne;
+                _spacecostofOne = Math.Ceiling(_spacecostofOne * 2);
+                _spaceinOne++;
+            }
+        }
+
+        private void reward2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        //Workstation 2
+    }
+}
